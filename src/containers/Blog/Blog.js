1def0551dc08f6afbd859b796d5f8cd47cf0f715
Rahul Tiwari <rahultiwari@Rahuls-MBP.lan1>
2018-12-28 00:12:45 -0500

    Code/Splitting - Custom Auth - Stack Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,65 +1,57 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const lazyLoading = asyncComponent(()=> {
+
+    return import('./NewPost/NewPost');
+})
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
 
-    componentDidMount () {
-        axios.get( '/posts' )
-            .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log( response );
-            } )
-            .catch(error => {
-                // console.log(error);
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+    state ={
+        auth: true
     }
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blogs">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    {/* implementing Guard functionality */}
+                    {this.state.auth ? <Route path="/new-post" component={lazyLoading} /> : null}
+                    <Route path="/posts" component={Posts} />
+                </Switch>
+                <h1 align="center"> Welcome to Rahul's Blog</h1>
+                <p align="center" > Rahul's Blog was developed as a side project while I was learning ReactJS , NodeJS and MongoDB. 
+                    <br></br>In this project , I have incorporated various concepts of Full-Stack Development like Routing <br></br>, Server Callbacks , Redux(For state management) and so on.
+
+                </p>
             </div>
         );
     }
